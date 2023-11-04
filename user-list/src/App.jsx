eb5404a5eb217446d4a61d92233d633c63e86184
Radoslav Kehadzkiyski <130061726+radito1@users.noreply.github.com>
2023-11-04 16:03:25 +0200

    add Footer component

--- user-list/src/App.jsx
+++ user-list/src/App.jsx
@@ -1,12 +1,14 @@
-import './styles.css';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import "./styles.css";
 
 function App() {
   return (
     <div>
-      Hello
+      <Header />
+      <Footer />
     </div>
-  )
-
+  );
 }
 
-export default App
+export default App;
