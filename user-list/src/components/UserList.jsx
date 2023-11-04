56bbd89a6f195390b55f5a70218d32dbc33b853a
Radoslav Kehadzkiyski <130061726+radito1@users.noreply.github.com>
2023-11-04 18:30:59 +0200

    add UserListTable component

--- user-list/src/components/UserList.jsx
+++ user-list/src/components/UserList.jsx
@@ -1,9 +1,11 @@
 import Search from "./Search";
+import UserListTable from "./UserListTable";
 
 const UserList = () => {
     return (
         <section className="card users-container">
             <Search />
+            <UserListTable/>
         </section>
     );
 };
