5d7226d92d03c10cc0b64e5483d0e822b9c2fe62
Radoslav Kehadzkiyski <130061726+radito1@users.noreply.github.com>
2023-11-05 16:56:27 +0200

    show and hide modal

--- user-list/src/components/UserListTable.jsx
+++ user-list/src/components/UserListTable.jsx
@@ -5,6 +5,7 @@ import * as userService from "../services/userService";
 
 const UserListTable = () => {
     const [users, setUsers] = useState([]);
+    const [showCreate, setShowCreate] = useState(false);
 
 
     useEffect(() => {
@@ -13,11 +14,17 @@ const UserListTable = () => {
     }, []);
 
     const createUserClickHandler = () => {
-        return;
+        setShowCreate(true);
+    }
+
+    const hideCreateUserModal = () => {
+        setShowCreate(false);
     }
 
     return (
         <div className="table-wrapper">
+            {showCreate && <CreateUserModal hideModal={hideCreateUserModal}/>}
+
             <table className="table">
                 <thead>
                     <tr>
@@ -68,6 +75,7 @@ const UserListTable = () => {
                                 <path fill="currentColor"
                                     d="M374.6 310.6l-160 160C208.4 476.9 200.2 480 192 480s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 370.8V64c0-17.69 14.33-31.1 31.1-31.1S224 46.31 224 64v306.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0S387.1 298.1 374.6 310.6z">
                                 </path>
+                    
                             </svg>
                         </th>
                         <th>Actions</th>
@@ -84,8 +92,6 @@ const UserListTable = () => {
             </table>
 
             <button className="btn-add btn" onClick={createUserClickHandler}>Add new user</button>
-
-            <CreateUserModal/>
         </div>
     );
 }
